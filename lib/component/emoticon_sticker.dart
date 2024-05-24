75ca01b1053c0d2af56f9bfdc04304ae9bf28fa8
siwoo Lee <leeseewoo@gmail.com>
2024-05-24 13:21:58 +0900

    Done : 240524 1PM

--- lib/component/emoticon_sticker.dart
+++ lib/component/emoticon_sticker.dart
@@ -5,9 +5,12 @@ class EmoticonSticker extends StatefulWidget {
   final VoidCallback onTransform; // 6
   final String imgPath; // 2
 
+  final bool isSelected;
+
   const EmoticonSticker({
     required this.onTransform,
     required this.imgPath,
+    required this.isSelected,
     Key? key,
   }) : super(key: key);
 
@@ -16,7 +19,6 @@ class EmoticonSticker extends StatefulWidget {
 }
 
 class _EmoticonStickerState extends State<EmoticonSticker> {
-
   double scale = 1; // 확대/축소 비율
   double hTransform = 0;
   double vTransform = 0;
@@ -25,26 +27,49 @@ class _EmoticonStickerState extends State<EmoticonSticker> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return GestureDetector(
-      onTap: () {
-        // 3
-        widget.onTransform(); // 6
-      },
-      onScaleUpdate: (ScaleUpdateDetails details) {
-        // 4
-        widget.onTransform(); // 6
-        setState(() {
-          scale = details.scale * actualScale;
-          vTransform += details.focalPointDelta.dy;
-          hTransform += details.focalPointDelta.dx;
-        });
-      },
-      onScaleEnd: (ScaleEndDetails details) {
-        actualScale = scale;
-      },
-      child: Image.asset(
-        widget.imgPath, //
-      ),
+    return Transform(
+      transform: Matrix4.identity()
+        ..translate(hTransform, vTransform)
+        ..scale(scale, scale),
+        child : Container(
+          decoration: widget.isSelected
+              ? BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            border: Border.all(
+              color: Colors.blue,
+              width: 1.0,
+            ),
+          )
+              : BoxDecoration(
+            border: Border.all(
+              width: 1.0,
+              color: Colors.transparent,
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              // 3
+              widget.onTransform(); // 6
+            },
+            onScaleUpdate: (ScaleUpdateDetails details) {
+              // 4
+              widget.onTransform(); // 6
+              setState(() {
+                scale = details.scale * actualScale;
+                vTransform += details.focalPointDelta.dy;
+                hTransform += details.focalPointDelta.dx;
+              });
+            },
+            onScaleEnd: (ScaleEndDetails details) {
+              actualScale = scale;
+            },
+            child: Image.asset(
+              widget.imgPath, //
+            ),
+          ),
+        )
     );
+
+
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:image_editor/component/main_app_bar.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:image_editor/model/sticker_model.dart';
+import 'package:image_editor/component/emoticon_sticker.dart';
+import 'package:uuid/uuid.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
+import 'dart:typed_data';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
+
+
 
 import '../component/footer.dart';
 
@@ -14,6 +24,10 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   XFile? image;
+
+  Set<StickerModel> stickers = {};    //화면에 추가된 스티커를 저장할 변수
+  String? selectedId;     // 현재 선택된 스티커의 ID
+  GlobalKey imgKey = GlobalKey();    // 이지미로 전환할 위젯에 입력해줄 키값
 
   @override
   Widget build(BuildContext context) {
@@ -45,13 +59,33 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget renderBody() {
     if (image != null) {
-      return Positioned.fill(
+      return RepaintBoundary(
+        key: imgKey,
+        child : Positioned.fill(
         child: InteractiveViewer(
-          child: Image.file(
-            File(image!.path),
-            fit: BoxFit.cover,
+          child: Stack(
+            fit: StackFit.expand,   // 크기 최대로 늘려주기
+            children: [
+              Image.file(
+                File(image!.path),
+                fit: BoxFit.cover,    // 이미지 최대한 공간 차지하게 하기
+              ),
+              ...stickers.map(
+                  (sticker) => Center(  // 최초 스티커 선택 시 중아에 배치
+                    child: EmoticonSticker(
+                      key: ObjectKey(sticker.id),
+                      onTransform: () {
+                        onTransform(sticker.id);
+                      },
+                      imgPath: sticker.imgPath,
+                      isSelected: selectedId == sticker.id,
+                    ),
+                  ),
+              ),
+            ],
           ),
         ),
+      ),
       );
     } else {
       return Center(
@@ -66,6 +100,13 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  void onTransform(String id) {
+    // 스티커가 변형될 때마다 변형 중인 스티커를 현재 선택한 스티커로 지정
+    setState(() {
+      selectedId = id;
+    });
+  }
+
   void onPickImage() async {
     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
 
@@ -74,8 +115,38 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void onSaveImage() {}
-  void onDeleteItem() {}
+  void onSaveImage() async {
+    RenderRepaintBoundary boundary = imgKey.currentContext!
+        .findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
 
-  void onEmoticonTap(int index) {}
+    Uint8List pngBytes = byteData!.buffer.asUint8List();
+
+    await ImageGallerySaver.saveImage(pngBytes, quality: 100);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('저장되었습니다.')),
+    );
+
+  }
+
+  void onDeleteItem() {
+    setState(() {
+      stickers = stickers.where((sticker) => sticker.id != selectedId).toSet();
+      // 현재 선택되 있는 스티커 삭제 후 Set로 변환
+    });
+  }
+
+  void onEmoticonTap(int index) async {
+    setState(() {
+      stickers = {
+        ...stickers,
+        StickerModel(
+          id: Uuid().v4(),      // 스티커의 고유ID
+          imgPath: 'asset/img/emoticon_$index.png',
+        ),
+      };
+    });
+  }
 }
